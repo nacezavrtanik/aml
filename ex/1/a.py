33e6a452dbf32de1f2868a796cbd99e2bb139fd2
Nace Zavrtanik <nace1337@gmail.com>
2023-02-24 19:55:50 +0100

    ex1a task 3

--- ex/1/a.py
+++ ex/1/a.py
@@ -1,26 +1,25 @@
 import pandas as pd
 
-# 1 Load data
-data = pd.read_csv('podatki.csv')
 
-# 2 Print basic information
-print('Rows, Columns:\n', data.shape, '\n')
-print('Data Types:\n', data.dtypes, '\n')
-print('Statistics:\n', data.describe(), '\n')
-print('NaN Values per Column:\n', data.isnull().sum(), '\n')
+if __name__ == '__main__':
 
+    # 1 Load data
+    data = pd.read_csv('podatki.csv')
 
-# 3 Handle missing values
-def prune_dataset(df, ratio=0.2):
+    # 2 Print basic information
+    print('Rows, Columns:\n', data.shape, '\n')
+    print('Data Types:\n', data.dtypes, '\n')
+    print('Statistics:\n', data.describe(), '\n')
+    print('NaN Values per Column:\n', data.isnull().sum(), '\n')
 
-    r, _ = df.shape
-    nans = data.isnull().sum()
+    # 3 Handle missing values
 
-    cols = []
-    for c, n in zip(df.columns, nans):
-        if n / r <= ratio:
-            cols.append(c)
+    # Drop columns with more than 1/5 NaN values
+    rows, _ = data.shape
+    threshold = int(rows - rows / 5)
+    data.dropna(axis=1, thresh=threshold, inplace=True)
 
-    df = df[cols]
-
-    return df
+    # Fill remaining Nan values with mean (numeric) or modus (other)
+    fillers = dict(data.mean(numeric_only=True))
+    fillers['X4'] = data['X4'].mode()[0]
+    data.fillna(value=fillers, inplace=True)
