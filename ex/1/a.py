b7b56e6581004026d93c38ad8ef4b23a876ad119
Nace Zavrtanik <nace1337@gmail.com>
2023-02-25 15:49:36 +0100

    ex1a task 4

--- ex/1/a.py
+++ ex/1/a.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib as mpl
+
+mpl.use('TkAgg')  # To avoid TypeError when calling pd.DataFrame.hist in PyCharm
 
 
 if __name__ == '__main__':
@@ -6,20 +9,29 @@ if __name__ == '__main__':
     # 1 Load data
     data = pd.read_csv('podatki.csv')
 
-    # 2 Print basic information
-    print('Rows, Columns:\n', data.shape, '\n')
-    print('Data Types:\n', data.dtypes, '\n')
-    print('Statistics:\n', data.describe(), '\n')
-    print('NaN Values per Column:\n', data.isnull().sum(), '\n')
+    # 2 Extract basic information
+    rows, columns = data.shape
+    data_types = data.dtypes
+    stats = data.describe()
+    nans = data.isnull().sum()
 
-    # 3 Handle missing values
+    # 3 Handle NaN values
 
     # Drop columns with more than 1/5 NaN values
-    rows, _ = data.shape
     threshold = int(rows - rows / 5)
     data.dropna(axis=1, thresh=threshold, inplace=True)
+    del threshold
 
     # Fill remaining Nan values with mean (numeric) or modus (other)
     fillers = dict(data.mean(numeric_only=True))
     fillers['X4'] = data['X4'].mode()[0]
     data.fillna(value=fillers, inplace=True)
+    del fillers
+
+    # 4 Visualise data
+
+    # Plot histogram
+    # data.hist(legend=True)
+
+    # Create correlation matrix
+    corr = data.corr()
