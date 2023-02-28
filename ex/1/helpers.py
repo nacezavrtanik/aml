8884999ec8b016bfd94e6b6583dae318a51f1d3a
Nace Zavrtanik <nace1337@gmail.com>
2023-02-28 17:58:58 +0100

    ex1b task 4

---
+++ ex/1/helpers.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_validation_curve(train_scores,
+                          test_scores,
+                          param_range,
+                          title='Validation Curve',
+                          xlabel='Hyperparameter',
+                          linewidth=2):
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    fig, ax = plt.subplots()
+
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel("Score")
+    ax.set(ylim=(0.0, 1.1))
+
+    ax.plot(
+        param_range, train_scores_mean, label="Training score", color="darkorange", lw=linewidth)
+    plt.fill_between(
+        param_range,
+        train_scores_mean - train_scores_std,
+        train_scores_mean + train_scores_std,
+        alpha=0.2,
+        color="darkorange",
+        lw=linewidth)
+
+    ax.plot(
+        param_range, test_scores_mean, label="Cross-validation score", color="navy", lw=linewidth)
+    plt.fill_between(
+        param_range,
+        test_scores_mean - test_scores_std,
+        test_scores_mean + test_scores_std,
+        alpha=0.2,
+        color="navy",
+        lw=linewidth)
+
+    plt.legend(loc="best")
+    plt.show()
