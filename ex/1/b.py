6e115c8865e6009d54de2dfd4467ec92715f89d0
Nace Zavrtanik <nace1337@gmail.com>
2023-02-28 15:16:02 +0100

    ex1b task 3

--- ex/1/b.py
+++ ex/1/b.py
@@ -1,4 +1,5 @@
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 from a import col_trans
 
 
@@ -8,4 +9,9 @@ X, y = col_trans[:, 0:-1], col_trans[:, -1]
 knc.fit(X, y)
 
 # 2 Evaluate accuracy of model
-print('Score:', knc.score(X, y))
+print('Score (entire dataset):', knc.score(X, y))
+
+# 3 Split dataset into train data and test data, retrain, and reevaluate
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=0)
+knc.fit(X_train, y_train)
+print('Score (train/test split):', knc.score(X_test, y_test))
